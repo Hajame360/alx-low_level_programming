38003b397ac8d56ba88ab7f28435976b9a303d4a
Hajame Phiri <hajamephiri7@gmail.com>
2022-09-12 06:56:09 -0700

    print from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print the string in the main function
+*
+* Description: using the main function
+* this program prints integers from 0-9 respectively
+* Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
