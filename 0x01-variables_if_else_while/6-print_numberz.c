2dc1a903297802166f8b76be7d9152ef09ce7704
Hajame Phiri <hajamephiri7@gmail.com>
2022-09-12 07:10:29 -0700

    purchar from 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+* main - print the string from the main function
+*
+* Description: using putchar to print numbers
+* this program printssingle digit numbers using putchar
+* Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
