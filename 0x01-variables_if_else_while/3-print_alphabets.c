65d4a9e7ef9a21cdd1d2cff51aa182f7822b4c5a
Hajame Phiri <hajamephiri7@gmail.com>
2022-09-12 05:59:33 -0700

    printing a to z from low to up

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints alphabets from A to Z in small then upper
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch = 'z' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A' ; ch = 'Z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
